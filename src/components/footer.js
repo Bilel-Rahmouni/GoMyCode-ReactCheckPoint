d678168f74076f9a36fa4e2640f0ec32472e974b
bilelrahmouni <bilelrahmouni01@hotmail.fr>
2020-06-17 16:45:57 +0100

    committing for the first time

---
+++ src/components/footer.js
@@ -0,0 +1,10 @@
+import React from "react";
+import "./footer.css";
+export default function footer() {
+  return (
+    <div className="footer">
+      <p>Da happy page</p>
+      <p>created by me. 2020</p>
+    </div>
+  );
+}
---
+++ src/components/giphy.js
@@ -0,0 +1,21 @@
+import React from "react";
+
+export default function giphy() {
+  return (
+    <div>
+      <iframe
+        src="https://giphy.com/embed/14udF3WUwwGMaA"
+        width="280"
+        height="190"
+        frameBorder="0"
+        class="giphy-embed"
+        allowFullScreen
+      ></iframe>
+      <p>
+        <a href="https://giphy.com/gifs/happy-movie-dancing-14udF3WUwwGMaA">
+          via GIPHY
+        </a>
+      </p>
+    </div>
+  );
+}
